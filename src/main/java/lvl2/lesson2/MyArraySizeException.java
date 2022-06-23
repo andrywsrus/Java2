021dd9fb9283077ffc9c29e6e2d4b31252b6bd18
Andrey <andryws@mail.ru>
2022-06-23 19:58:34 +0300

    Домашнее задание, урок 2(переделано)

--- src/main/java/lvl2/lesson2/MyArraySizeException.java
+++ src/main/java/lvl2/lesson2/MyArraySizeException.java
@@ -1,7 +1,8 @@
 package lvl2.lesson2;
 
-public class MyArraySizeException extends Exception {
-    public MyArraySizeException(String message) {
-        super(message);
+public class MyArraySizeException extends MyException {
+
+    public MyArraySizeException(){
+        super("Ошибка в размерности массива, необходимо 4x4");
     }
 }
---
+++ src/main/java/lvl2/lesson2/MyException.java
@@ -0,0 +1,8 @@
+package lvl2.lesson2;
+
+public class MyException extends Exception {
+
+    public MyException(String message){
+        super(message);
+    }
+}
