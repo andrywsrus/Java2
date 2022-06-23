af0920072b0520cb0a481c1fd99bfba6bbc25f95
andrywsrus <105842477+andrywsrus@users.noreply.github.com>
2022-06-23 20:03:36 +0300

    Delete src/main/java/lvl2/lesson2 directory

--- src/main/java/lvl2/lesson2/MyArrayDataException.java
+++
@@ -1,8 +0,0 @@
-package lvl2.lesson2;
-
-public class MyArrayDataException extends MyException {
-
-    public MyArrayDataException(int x, int y){
-        super("В индексе [" + (x) + "][" + (y) + "] массива не число");
-    }
-}
--- src/main/java/lvl2/lesson2/MyArraySizeException.java
+++
@@ -1,8 +0,0 @@
-package lvl2.lesson2;
-
-public class MyArraySizeException extends MyException {
-
-    public MyArraySizeException(){
-        super("Ошибка в размерности массива, необходимо 4x4");
-    }
-}
